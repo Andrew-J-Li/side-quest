d852cbea41cc5f16beeff9c9444e180bf8e7ee14
Hantao <hantaojin@gmail.com>
2024-11-03 01:26:18 -0600

    Fixed server

--- server/server.js
+++ server/server.js
@@ -2,8 +2,20 @@ import express from "express"
 import cors from "cors"
 import users from "./routes/user.js"
 
+import path from 'path'
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const PORT = process.env.PORT || 5050
 const app = express();
+
+app.use(express.static(path.join(__dirname, '../client/build')))
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.use(cors())
 app.use(express.json())
