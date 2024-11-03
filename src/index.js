2464b941689c44182f4a3e02394176355c7015ca
Jason X <jasonkaixie@gmail.com>
2024-11-02 22:45:04 -0500

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
