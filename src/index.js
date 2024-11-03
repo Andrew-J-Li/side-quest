384011b918987b8fda1fbc4c247971116d3d893e
Zafir <zafir.nasim@gmail.com>
2024-11-03 02:18:15 -0600

    commit to include Home.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Home from '../src/components/Home';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
@@ -10,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-      <Home />
+      <App />
     </HashRouter>
   </React.StrictMode>
 );
